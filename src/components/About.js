8083ca0b94fec13f396df28cb8f24a86f3b28904
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-21 19:51:56 -0700

    commit tree 8d84f88ca597f0107610164decedb1c851035d6a

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">About
+  <h2>About Me</h2>
+  <p>Took me a bit to Google the syntax of styling in JSX</p>
+  <img src={image} alt="I made this"/>  
+  </div>
+    );
 }
 
 export default About;
